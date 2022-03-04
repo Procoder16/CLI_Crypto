dfbe80a88814e2132ec1270f3d49ef68c7f99411
Procoder16 <smsoumik1608@gmail.com>
2022-03-05 02:02:59 +0530

    project completed

--- bin/coindex.js
+++ bin/coindex.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-
 const program = require('commander');
 const pkg = require('../package.json');
 
 program
-    .version(pkg.version)
-    .command('key', 'Manage API Key -- https://nomics.com')
-    .parse(process.argv);
+  .version(pkg.version)
+  .command('key', 'Manage API Key -- https://nomics.com')
+  .command('check', 'Check Coin Price Info')
+  .parse(process.argv);
